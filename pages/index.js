8d51e63ee6f70ebe0e2d2793141600360c42c4f0
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2022-04-20 22:23:15 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
-export default function Blog() {
+export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
