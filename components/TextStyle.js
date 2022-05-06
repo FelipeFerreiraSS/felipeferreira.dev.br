8febba27152b24503eff5637a9720cbe60312896
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2022-05-05 22:47:10 -0300

    creating components

---
+++ components/TextStyle.js
@@ -0,0 +1,9 @@
+const TextStyle = ({ text }) => {
+  return (
+    <strong style={{color: 'red',fontSize: '20px'}}>
+      {text}
+    </strong>
+  )
+}
+
+export default TextStyle
---
+++ components/YouTube.js
@@ -0,0 +1,16 @@
+export default function YouTube({ id }) {
+  return (
+    <div>
+      <iframe 
+        width="660" 
+        height="350" 
+        src={`https://www.youtube.com/embed/${id}`} 
+        title="YouTube video player" 
+        frameBorder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+        allowFullScreen="true"
+        >
+        </iframe>
+    </div>
+  );
+}
