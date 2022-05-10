02cf66dc8b20840349a73722cf73671690ce2bc3
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2022-05-09 21:44:47 -0300

    Update YouTube.js

--- components/YouTube.js
+++ components/YouTube.js
@@ -8,7 +8,7 @@ export default function YouTube({ id }) {
         title="YouTube video player" 
         frameBorder="0" 
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-        allowFullScreen="true"
+        allowFullScreen={true}
         >
         </iframe>
     </div>
