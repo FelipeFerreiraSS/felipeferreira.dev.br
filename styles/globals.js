6ede59d296966a8ace71a58c60cd3e478754d174
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2022-05-13 19:29:47 -0300

    Update globals.js

--- styles/globals.js
+++ styles/globals.js
@@ -8,7 +8,12 @@ export default createGlobalStyle`
 	  	padding: 0;
 	  	margin: 0;
 	  	font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-	    	Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+	    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+		
+		display: flex;
+		justify-content: center;
+		align-items: center;
+
 	}
 	
 	a {
