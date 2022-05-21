d9294c1f481c663dbcf28bb91489769c0f510e1f
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2022-05-21 20:13:04 -0300

    Update Code.js

--- components/Code.js
+++ components/Code.js
@@ -2,15 +2,12 @@ import React, { useEffect } from "react";
 import Prism from "prismjs";
 import "prismjs/themes/prism-tomorrow.css";
 
-export default function Code({ code, language, nameLanguage }) {
+export default function Code({ code, language }) {
   useEffect(() => {
     Prism.highlightAll();
   }, []);
   return (
     <div>
-      <p style={{color: 'white',fontSize: '20px', background: '#2D2D2D', width: '100px'}}>
-        {nameLanguage}
-      </p>
       <pre>
         <code className={`language-${language}`}>{code}</code>
       </pre>
