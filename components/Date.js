7796180f41a37836cc5556cf4e172f2702e5da21
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2022-05-21 20:13:13 -0300

    Update Date.js

--- components/Date.js
+++ components/Date.js
@@ -2,5 +2,5 @@ import { parseISO, format } from 'date-fns'
 
 export default function Date({ dateString }) {
   const date = parseISO(dateString)
-  return <time dateTime={dateString}>{format(date, 'dd/LL/yyyy')}</time>
+  return <time dateTime={dateString}>📅{format(date, 'dd/LL/yyyy')}</time>
 }
