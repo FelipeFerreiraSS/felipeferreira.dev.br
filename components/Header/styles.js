869d96540a4753aa3af90ea7bf515acf0f178168
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2022-05-26 23:50:46 -0300

    add media queries part 1

--- components/Header/styles.js
+++ components/Header/styles.js
@@ -4,36 +4,61 @@ export const NavBar = styled.div`
   display: flex;
   justify-content: space-between;
   width: 1200px;
+
+  @media(max-width: 1030px) {
+    width: 900px;
+  }
+
+  @media(max-width: 770px) {
+    width: 800px;
+  }
+
+  @media(max-width: 430px) {
+    width: 600px;
+  }
 `;
 
 export const Avatar = styled.div`
-    width: 50%;
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-    cursor: pointer;
-    
-    img {
-      border-radius: 100%;
-    }
+  width: 50%;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  cursor: pointer;
+  
+  img {
+    border-radius: 100%;
+  }
+  h2 {
+    padding-left: 20px;
+    font-size: 30px;
+  }
 
-    h2 {
-        padding-left: 20px;
-        font-size: 30px;
-    }
+  @media(max-width: 770px) {
+    padding-left: 40px;
+  }
+
+  @media(max-width: 430px) {
+    width: 70%;
+    margin-left: 180px;
+  }
 `
 
 export const Menu = styled.div`
-    width: 50%;
-    ul {
+  width: 50%;
+  ul {
     display: flex;
     justify-content: flex-end;
     align-items: center;
-    }
-    li {
-        padding: 20px;
-        font-size: 20px;
-        list-style-type: none;
-    }
+  }
+  li {
+    padding: 20px;
+    font-size: 20px;
+    list-style-type: none;
+  }
+
+  @media(max-width: 430px) {
+    display: none;
+    width: 30%;
+  }
     
 `
