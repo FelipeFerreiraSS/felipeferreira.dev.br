61a1739b95d95bf6bab0a50a5592b707ed73f042
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2022-05-28 20:23:09 -0300

    add media queries

--- styles/blog.js
+++ styles/blog.js
@@ -9,6 +9,38 @@ export const DisplaysPosts = styled.div`
         margin-right: 20px;
     }
 
+    @media(max-width: 1030px) {
+        width: 900px;
+    }
+
+    @media(max-width: 770px) {
+        width: 740px;
+
+        img {
+            width: 300px;
+        }
+    }
+
+    @media(max-width: 430px) {
+        width: 500px;
+        margin-left: 230px;
+        display: flex;
+        flex-direction: column;
+
+        img {
+            width: 500px;
+        }
+    }
+
+    @media(max-width: 380px) {
+        width: 480px;
+        margin-left: 250px;
+
+        img {
+            width: 480px;
+        }
+    }
+
 `;
 
 export const DisplaysDescription = styled.div`
