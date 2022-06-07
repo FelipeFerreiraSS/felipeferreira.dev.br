9a8ea6cf5666d1440321f09669e4c267e59ee4f9
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2022-06-07 19:16:57 -0300

    add media queries 770 and 400px home page

--- styles/home.js
+++ styles/home.js
@@ -2,26 +2,91 @@ import styled from 'styled-components'
 
 export const Main = styled.main`
 
+  @media(max-width: 1030px) {
+    width: 900px;
+  }
+  
+  @media(max-width: 770px) {
+    width: 700px;
+    margin: auto;
+  }
+
+  @media(max-width: 400px) {
+    width: 350px;
+    margin: auto;
+  }
+
+  @media(max-width: 330px) {
+    
+  }
 `;
 
 // Principal
 export const Cover = styled.section`
   height: 550px;
   display: flex;
+
+  @media(max-width: 1030px) {
+    height: 400px;
+  }
+
+  @media(max-width: 400px) {
+    height: 330px;
+  }
 `;
 
 export const Description = styled.div`
   width: 50%;
-  margin-top: 80px;
+  margin-top: 60px;
 
   h1 {
-      font-size: 80px;
-      line-height: 0.2;
-
+    font-size: 80px;
+    line-height: 0.2;
   }
 
   h3 {
+    font-size: 30px;
+  }
+
+  @media(max-width: 1030px) {
+    margin-top: 30px;
+  
+    h1 {
+      font-size: 60px;
+      width: 450px;
+    }
+  }
+
+  @media(max-width: 770px) {
+    margin-top: 0px;
+
+    h1 {
+      font-size: 50px;
+      width: 350px;
+    }
+
+    h3 {
       font-size: 30px;
+      width: 250px;
+    }
+  }
+
+  @media(max-width: 400px) {
+    width: 100%;
+    margin-top: 30px;
+    text-align: center;
+
+    h3 {
+      font-size: 27px;
+      width: 100%;
+    }
+
+    div {
+      button {
+        margin: 10px;
+        margin-top: 20px;
+      }
+    }
   }
 `;
 
@@ -49,7 +114,15 @@ export const Button = styled.div`
 `
 
 export const Img = styled.div`
-    margin-left: 90px;
+  margin-left: 90px;
+
+  @media(max-width: 770px) {
+    margin-left: 0px;
+  }
+
+  @media(max-width: 400px) {
+    display: none;
+  }
 `
 
 // Sobre
@@ -65,10 +138,45 @@ export const About = styled.section`
     width: 400px;
     font-size: 20px;
   }
+
+  @media(max-width: 400px) {
+    flex-direction: column;
+    height: 1050px;
+
+    h2 {
+      text-align: center;
+    }
+
+    p {
+      width: 100%;
+    }
+  }
 `
 
 export const AboutMe = styled.div`
   width: 50%;
+
+  @media(max-width: 770px) {
+    p {
+      width: 300px;
+    }
+  }
+
+  @media(max-width: 400px) {
+    width: 100%;
+
+    p {
+      width: 350px;
+      text-align: center;
+    }
+
+    div {
+      button {
+        margin: 13px;
+        margin-top: 20px;
+      }
+    }
+  }
 `
 
 export const SocialIcons = styled.div`
@@ -88,37 +196,41 @@ export const SocialIcons = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
-
   }
-`
 
-export const Resume = styled.div`
-  button {
-      width: 140px;
-      height: 40px;
-      font-size: 18px;
-      margin-right: 20px;
-      margin-top: 20px;
-      cursor: pointer;
-      background-color: ${({theme}) => theme.colors.blue};
-      color: ${({theme}) => theme.colors.text};
-      border: none;
-      border-radius: 10px;
-      transition: 0.6s;
-
-      &:hover,
-      &:focus{
-        background-color: ${({theme}) => theme.colors.blueHover};
-        transform: translateY(-0.25em);
-      }
+  @media(max-width: 400px) {
+    ul {
+      justify-content: center;
+    }
   }
 `
 
 export const Knowledge = styled.div`
   width: 50%;
+
+  @media(max-width: 400px) {
+    h2 {
+      width: 350px;
+      text-align: center;
+    }
+  }  
 `
 export const Technology = styled.div`
   width: 500px;
+
+  @media(max-width: 1030px) {
+    width: 450px;
+  }
+
+  @media(max-width: 770px) {
+    width: 370px;
+    margin-left: -20px;
+  }
+
+  @media(max-width: 400px) {
+    width: 350px;
+    margin: auto;
+  }
 `
 export const TechnologyGrid = styled.div`
   display: flex;
@@ -155,7 +267,15 @@ export const Projects = styled.section`
     text-align: center;
     font-size: 45px;
   }
-  
+
+  @media(max-width: 400px) {
+    height: 850px;
+
+    h2 {
+      width: 350px;
+      text-align: center;
+    }
+  }
 `
 
 export const AllProjects = styled.div`
@@ -173,6 +293,46 @@ export const AllProjects = styled.div`
     width: 450px;
     height: 218px;
   }
+
+  @media(max-width: 770px) {
+    img {
+      width: 300px;
+      height: 170px;
+    }
+
+    p {
+      width: 300px;
+    }
+  }
+
+  @media(max-width: 400px) {
+    flex-direction: column;
+
+    div {
+      width: 100%;
+    }
+
+    img {
+      margin-left: 30px;
+    }
+
+    h3 {
+      font-size: 22px;
+      text-align: center;
+    }
+
+    p {
+      width: 350px;
+      text-align: center;
+    }
+
+    a {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin-bottom: 30px;
+    }
+  }
 `
 
 // LatestPosts
@@ -183,12 +343,19 @@ export const Posts = styled.section`
   h2 {
     font-size: 45px;
   }
+
+  @media(max-width: 400px) {
+    height: 1400px;
+
+    h2 {
+      text-align: center;
+    }
+  }
   
 `
 
 export const LatestPosts = styled.div`
   display: flex;
-  
 `
 
 export const DisplayingPosts = styled.div`
@@ -206,6 +373,20 @@ export const DisplayingPosts = styled.div`
     border-radius: 15px;
   }
 
+  @media(max-width: 1030px) {
+    width: 100%;
+  }
+
+  @media(max-width: 400px) {
+    div {
+      flex-direction: column;
+      align-items: center;
+    }
+
+    img {
+      margin: auto;
+    }
+  }
   
 `
 
@@ -243,16 +424,81 @@ export const DescriptionPost = styled.div`
     align-items: center;
     justify-content: space-around;
   }
+
+  @media(max-width: 770px) {
+    h3 {
+      max-width: 400px;
+    }
+
+    p {
+      max-width: 400px;
+    }
+
+    span {
+      max-width: 200px;
+    }
+  }
+
+  @media(max-width: 400px) {
+    h3 {
+      text-align: center;
+    }
+
+    p {
+      max-width: 350px;
+      text-align: center;
+    }
+
+    div {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: row;
+      width: 350px;
+
+      time {
+        width: 120px;
+        margin-right: 0px;
+      }
+
+      span {
+        width: 150px;
+      }
+    }
+  }
+
 `
 export const AllPosts = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+
+  @media(max-width: 1030px) {
+    width: 900px;
+  }
+
+  @media(max-width: 770px) {
+    width: 700px;
+  }
+
+  @media(max-width: 400px) {
+    width: 350px;
+    div {
+      button {
+        margin: auto;
+        margin-top: -10px;
+      }
+    }
+  }
 `
 
 export const Tags = styled.div`
   width: 30%;
   text-align: center;
+
+  @media(max-width: 1030px) {
+    display: none;
+  }
 `
 
 export const AllTagsGrid = styled.div`
@@ -280,6 +526,10 @@ export const AllTagsGrid = styled.div`
     align-items: center;
     text-align: center;
   }
+
+  @media(max-width: 1030px) {
+    display: none;
+  }
 `
 
 // Contact 
@@ -295,12 +545,46 @@ export const Contact = styled.section`
   p {
     text-align: center;
   }
+
+  @media(max-width: 1030px) {
+    height: 600px;
+  }
+
+  @media(max-width: 400px) {
+    height: 700px;
+    
+
+    p {
+      
+    } 
+  }
   
 `
 
 export const BoxMenssages = styled.div`
   display: flex;
+  justify-content: center;
+  align-items: center;
   margin-top: 50px;
+
+  @media(max-width: 1030px) {
+    img {
+      width: 400px;
+    }
+  }
+
+  @media(max-width: 770px) {
+    img {
+      width: 300px;
+    }
+  }
+
+  @media(max-width: 400px) {
+    display: block;
+    img {
+      display: none;
+    }
+  }
 `
 
 export const Menssages = styled.div`
@@ -326,14 +610,13 @@ export const Menssages = styled.div`
     }
   }
 
-
-
   textarea{
     width: 100%;
     height: 270px;
     border-radius: 20px;
     border: solid ${({theme}) => theme.colors.text} 1px;
     background-color: ${({theme}) => theme.colors.background};
+    color: ${({theme}) => theme.colors.text};
     padding-left: 20px;
     padding-top: 10px;
     resize: none;
@@ -358,5 +641,16 @@ export const Menssages = styled.div`
       background-color: ${({theme}) => theme.colors.blueHover};
       transform: translateY(-0.25em);
     }
+  }
+
+  @media(max-width: 1030px) {
+    textarea {
+      height: 230px;
+    }
+  }
+
+  @media(max-width: 400px) {
+    width: 100%;
+    margin-left: 0px;
   }
 `
