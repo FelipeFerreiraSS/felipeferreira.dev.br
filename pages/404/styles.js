d3aa26d82eb84b1d2773833f27bbd8a91eea2f20
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2022-07-26 16:33:12 -0300

    add media queries 404 page

--- pages/404/styles.js
+++ pages/404/styles.js
@@ -5,11 +5,28 @@ export const Content = styled.div`
     display: flex;
     justify-content: space-around;
     align-items: center;
+
+    @media(max-width: 770px) {
+        justify-content: space-evenly;
+    }
+
+    @media(max-width: 400px) {
+        flex-direction: column-reverse;
+        height: 700px;
+    }
+
+    @media(max-width: 330px) {
+        
+    }
 `;
 
 export const Menssagem = styled.div`
     display: flex;
     flex-direction: column;
+
+    @media(max-width: 770px) {
+        
+    }
 
     h1 {
         font-size: 70px;
@@ -31,17 +48,42 @@ export const Menssagem = styled.div`
 
 export const Imagem = styled.div`
     display: flex;
+    align-items: center;
+    justify-content: center;
     flex-direction: column;
 
     img {
         width: 500px;
         margin-top: -30px;
+
+        @media(max-width: 770px) {
+            width: 400px;
+            margin-right: -100px;
+        }
+
+        @media(max-width: 400px) {
+            width: 350px;
+            margin-right: 0px;
+        }
+
+        @media(max-width: 330px) {
+            width: 300px;
+        }
     }
 
     a {
         font-size: 8px;
         color: ${({theme}) => theme.colors.gray};
-        margin: auto;
         margin-top: -80px;
+
+        @media(max-width: 770px) {
+            margin-top: -60px; 
+            
+        }
+
+        @media(max-width: 400px) {
+            margin-top: -40px; 
+            
+        }
     }
 `;
