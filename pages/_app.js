238d300345d2d407db158237c00dcd845ab59d1a
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2022-09-22 22:54:23 -0300

    app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
 
+import Navbar from '../components/NavBar'
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <> 
+      <Navbar />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
