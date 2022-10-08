a8c0aaae1c71f9755ac49275a7601edbac462844
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2022-10-07 23:23:44 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
 import '../styles/globals.css'
 
 import Navbar from '../components/NavBar'
+import Footer from '../components/Footer'
 
 function MyApp({ Component, pageProps }) {
   return (
     <> 
       <Navbar />
       <Component {...pageProps} />
+      <Footer />
     </>
   )
 }
