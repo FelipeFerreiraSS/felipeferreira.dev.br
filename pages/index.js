0bf89c2bc476a384fbb0735662891b4cf8924af4
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2022-10-31 23:02:50 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,54 @@
-import Link from 'next/link';
+import Presentation from '../components/home/Presentation';
+import About from '../components/home/About';
+import Projects from '../components/home/Projects'
+import LatestPosts from '../components/home/LatestPosts';
+import Contact from '../components/home/Contact';
 
-export default function Home() {
+import { GraphQLClient } from 'graphql-request';
+
+export default function Home({ posts }) {
 
   return (
     <>
-      <div>
-        <h1 class="text-3xl font-bold underline">
-          Home
-        </h1>
-        <div>
-          <Link href={"/blog/"}>
-              <a class="text-sky-500 hover:text-sky-600">Blog</a>
-          </Link>
-        </div>
-      </div>
+      <Presentation />
+      <About />
+      <Projects/>
+      <LatestPosts teste={posts}/>
+      <Contact />
     </>
   );
+}
+
+export async function getStaticProps() {
+  const hygraph = new GraphQLClient(process.env.NEXT_PUBLIC_CONTENT_API);
+
+  const { posts } = await hygraph.request(
+    `
+      {
+        posts(orderBy: date_DESC) {
+          id
+          tags
+          title
+          slug
+          coverImage {
+            url(transformation: {image: {resize: {width: 800, height: 400, fit: crop}}})
+          }
+          date
+          authors {
+            name
+            picture {
+              url(transformation: {image: {resize: {height: 50, width: 50}}})
+            }
+          }
+          description
+        }
+      }
+    `
+  );
+
+  return {
+    props: {
+      posts,
+    },
+  };
 }
