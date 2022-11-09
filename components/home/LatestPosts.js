3a2376404a55e6da887de301f5e9c075ece4fc87
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2022-11-09 20:17:34 -0300

    update components latestPosts

--- components/home/LatestPosts.js
+++ components/home/LatestPosts.js
@@ -1,13 +1,14 @@
 import Link from 'next/link';
+import Date from '../Date';
 
-export default function LatestPosts({ teste }) {
+export default function LatestPosts({ latestPosts }) {
   return (
     <section className='bg-gray-800 text-gray-300'>
       <div class="mx-auto max-w-screen-xl px-4 py-4 sm:py-16 sm:px-6 lg:px-8">
         <h2 class="text-3xl text-center font-bold sm:text-4xl">Últimos posts no blog</h2>
 
         <div className="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
-          {teste.map((post) => (
+          {latestPosts.map((post) => (
             <Link key={post.id} href={`/blog/${post.slug}`}>
               <div className="group relative cursor-pointer">
                 <div className="overflow-hidden rounded-lg bg-white group-hover:opacity-75 ">
@@ -17,13 +18,21 @@ export default function LatestPosts({ teste }) {
                     className="h-full w-full object-cover object-center"
                   />
                 </div>
-                <h1 className="mt-2 text-3xl text-gray-100">
+                <h1 className="mt-2 text-3xl text-gray-100 font-semibold text-center lg:text-start">
                   {post.title}
                 </h1>
-                <p class="text-gray-100 text-xs tracking-widest font-semibold">
-                  {new Date(post.date).toDateString()}
-                </p>
-                <p className="text-sm font-semibold text-gray-300 mb-8 mt-2">{post.description}</p>
+                <div class="flex items-center justify-center lg:justify-start mt-2 mb-2">
+                  <p class="text-gray-100 text-xs tracking-widest mr-3">
+                    <Date dateString={post.date}/>
+                  </p>
+                  <p class="text-gray-100 text-xs tracking-widest mr-3">
+                    📌{post.tags}
+                  </p>
+                  <p class="text-gray-100 text-xs tracking-widest">
+                    🕐{post.readingTime}
+                  </p>
+                </div>
+                <p className="text-sm text-gray-300 mb-8 mt-2 text-center lg:text-start">{post.description}</p>
               </div>
             </Link>
           ))}
