c30e422903a9f5426ab76daeb2c5ec9fef1d5c30
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2022-11-09 20:21:41 -0300

    update blog page

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,42 +1,10 @@
-import Image from 'next/image'
-import Link from 'next/link';
 import { GraphQLClient } from 'graphql-request';
 
-import LinksTags from '../../components/LinksTags';
+import AllPosts from '../../components/AllPosts'
 
 export default function Posts({ posts }) {
   return (
-    <div>
-      <LinksTags />
-      <div className="bg-gray-800">
-        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-          <div className="mx-auto max-w-2xl py-10 sm:py-10 lg:max-w-none lg:py-10">
-            <div className="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
-              {posts.map((post) => (
-                <Link key={post.id} href={`/blog/${post.slug}`}>
-                  <div className="group relative cursor-pointer">
-                    <div className="overflow-hidden rounded-lg bg-white group-hover:opacity-75 ">
-                      <img
-                        src={post.coverImage.url}
-                        alt="teste"
-                        className="h-full w-full object-cover object-center"
-                      />
-                    </div>
-                    <h1 className="mt-2 text-3xl text-gray-100">
-                      {post.title}
-                    </h1>
-                    <p class="text-gray-100 text-xs tracking-widest font-semibold">
-                      {new Date(post.date).toDateString()}
-                    </p>
-                    <p className="text-sm font-semibold text-gray-300 mb-8 mt-2">{post.description}</p>
-                  </div>
-                </Link>
-              ))}
-            </div>
-          </div>
-        </div>
-      </div>   
-    </div>
+    <AllPosts allPosts={posts}/>
   );
 }
 
@@ -62,6 +30,7 @@ export async function getStaticProps() {
             }
           }
           description
+          readingTime
         }
       }
     `
