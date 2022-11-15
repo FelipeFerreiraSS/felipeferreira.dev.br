dc4bb1b59867888b4a731d395d621d0fbe898d1c
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2022-11-14 21:54:50 -0300

    Create nextJs.js

---
+++ pages/blog/tags/nextJs.js
@@ -0,0 +1,44 @@
+import { GraphQLClient } from 'graphql-request';
+
+import AllPosts from '../../../components/AllPosts'
+
+export default function NextJs({ posts }) {
+  return (
+    <AllPosts allPosts={posts}/>
+  );
+}
+
+export async function getStaticProps() {
+  const hygraph = new GraphQLClient(process.env.NEXT_PUBLIC_CONTENT_API);
+
+  const { posts } = await hygraph.request(
+    `
+      {
+        posts(where: {tags_contains_all: "nextJs"}, orderBy: date_DESC) {
+          id
+          tags
+          title
+          slug
+          coverImage {
+            url(transformation: {image: {resize: {width: 800, height: 400, fit: crop}}})
+          }
+          date
+          authors {
+            name
+            picture {
+              url(transformation: {image: {resize: {height: 50, width: 50}}})
+            }
+          }
+          description
+          readingTime
+        }
+      }
+    `
+  );
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
