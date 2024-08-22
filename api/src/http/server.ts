d93b3cb491e9d14dbd7de9fed2f1cf7778b50caf
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2024-08-21 21:06:33 -0300

    installing fastify

--- api/src/http/server.ts
+++ api/src/http/server.ts
@@ -1,1 +1,13 @@
-console.log("Server rodando!");
+import fastify from 'fastify'
+
+const app = fastify()
+
+const port = 3333
+
+app.get('/hello', () => {
+  return 'Hello World!!!'
+})
+
+app.listen({ port }).then(() => {
+  console.log(`Server rodando na porta: ${port}`)
+})
