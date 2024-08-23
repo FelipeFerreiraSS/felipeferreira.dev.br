bdd36cfd2855f00653ee93ea1495574151e4406a
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2024-08-22 22:21:10 -0300

    installing zod and creating route users

--- api/src/http/server.ts
+++ api/src/http/server.ts
@@ -1,11 +1,44 @@
 import fastify from 'fastify'
+import { z } from 'zod'
+import { PrismaClient } from '@prisma/client'
 
 const app = fastify()
 
 const port = 3333
 
-app.get('/hello', () => {
-  return 'Hello World!!!'
+const prisma = new PrismaClient()
+
+app.post('/users', async (request, reply) => {
+  const createUserBody = z.object({
+    firstName: z.string(),
+    lastName: z.string(),
+    email: z.string(),
+    password: z.string()
+  }) 
+
+  const { firstName, lastName, email, password } = createUserBody.parse(request.body)
+
+  const user = await prisma.user.create({
+    data: {
+      firstName,
+      lastName,
+      email,
+      type : "administrador",
+      password,
+    }
+  })
+
+  return reply.status(201).send({ newUser: user })
+})
+
+app.get('/users', async (request, reply) => {
+  try {
+    const users = await prisma.user.findMany()
+    return reply.status(200).send({ allUsers: users })
+  } catch (error) {
+    console.error('Error fetching users:', error)
+    return reply.status(500).send({ error: 'Internal Server Error' })
+  }
 })
 
 app.listen({ port }).then(() => {
