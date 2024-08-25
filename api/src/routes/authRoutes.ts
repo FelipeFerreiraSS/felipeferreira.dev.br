dcf40ac8af8819226cec618f85289231abd0a51f
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2024-08-25 20:27:45 -0300

    commenting routes

--- api/src/routes/authRoutes.ts
+++ api/src/routes/authRoutes.ts
@@ -2,5 +2,5 @@ import { FastifyInstance } from 'fastify';
 import { loginHandler } from '../controllers/authController';
 
 export const authRoutes = async (app: FastifyInstance) => {
-  app.post('/login', loginHandler);
+  //app.post('/login', loginHandler);
 };
