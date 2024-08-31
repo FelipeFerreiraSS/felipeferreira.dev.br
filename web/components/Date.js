9cfce6f0cf81e45cb90c6a75162bce05caa97161
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2024-08-31 19:30:54 -0300

    reset project

--- web/components/Date.js
+++
@@ -1,8 +0,0 @@
-import { parseISO, format } from 'date-fns'
-
-export default function Date({ dateString }) {
-  const date = parseISO(dateString)
-  return (
-    <time dateTime={dateString}>📅{format(date, 'd/LL/yyyy')}</time>
-  )
-}
