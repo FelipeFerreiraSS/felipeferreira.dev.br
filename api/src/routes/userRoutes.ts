0239735eeb4028b645058bea81b144431623484f
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2024-08-31 16:26:41 -0300

    uncommenting routes

--- api/src/routes/userRoutes.ts
+++ api/src/routes/userRoutes.ts
@@ -9,8 +9,7 @@ import { authenticateJWT } from '../middlewares/authenticateJWT';
 
 export const userRoutes = async (app: FastifyInstance) => {
   // Rotas protegidas por autenticação JWT
-  //app.post('/users', { preHandler: authenticateJWT }, createUserHandler);
-  app.post('/users', createUserHandler);
+  app.post('/users', { preHandler: authenticateJWT }, createUserHandler);
   app.get('/users', { preHandler: authenticateJWT }, getAllUsersHandler);
   app.put('/users/:id', { preHandler: authenticateJWT }, updateUserHandler);
   app.delete('/users/:id', { preHandler: authenticateJWT }, deleteUserHandler);
