5c7e83541926e5f1aee6ceda4f082b91595e3924
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2024-09-08 21:35:56 -0300

    table list posts

--- web/src/pages/dashboard/admin/index.tsx
+++ web/src/pages/dashboard/admin/index.tsx
@@ -22,39 +22,52 @@ export default function DashboardAdmin() {
     }
   }, [])
   return (
-    <>
-      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm mb-5">
-          <h1>Dashboard</h1>
-          <p>Bem vindo Administrador: {userState.user?.firstName} {userState.user?.lastName}</p>
-          <p>email: {userState.user?.email}</p>
-          <p>tipo: {userState.user?.type}</p>
-        </div>
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm mb-5">
-          <h2>Lista de Posts</h2>
-          <ul>
-            {postsState.posts?.map((post) => (
-              <div className="sm:mx-auto sm:w-full sm:max-w-sm mb-5" key={post.id}>
-                <p>{post.author.firstName}</p>
-                <p>{post.title}</p>
-                <p>{post.summary}</p>
-                <p>{post.headerImage?.imageUrl}</p>
-                {post.tags.map((tag) => (
-                  <div key={tag.id}>
-                    <li>{tag.name}</li>
-                  </div>
-                ))}
-              </div>
-            ))}
-          </ul>
-        </div>
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <Button onClick={() => signOut()}>
-            Sair
-          </Button>
-        </div>
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm mb-5">
+        <h1>Dashboard</h1>
+        <p>Bem vindo Administrador: {userState.user?.firstName} {userState.user?.lastName}</p>
+        <p>email: {userState.user?.email}</p>
+        <p>tipo: {userState.user?.type}</p>
       </div>
-    </>
+      <div className="mx-auto w-full max-w-4xl mb-5">
+        <h2>Lista de Posts</h2>
+        <table border={1} cellPadding="10" cellSpacing="0">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Autor</th>
+              <th>Capa</th>
+              <th>Titulo</th>
+              <th>Resumo</th>
+              <th>Tags</th>
+            </tr>
+          </thead>
+          <tbody>
+            {postsState.posts?.map((post) => (
+              <tr key={post.id} className="border-black border-2 ">
+                <td>{post.id}</td>
+                <td>{post.author.firstName}</td>
+                <td>{post.headerImage?.imageUrl}</td>
+                <td>{post.title}</td>
+                <td>{post.summary}</td>
+                <td className="w-36">
+                  {post.tags.map((tag) => (
+                    <div key={tag.id}>
+                      <li>{tag.name}</li>
+                    </div>
+                  ))}
+                </td>  
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <Button onClick={() => signOut()}>
+          Sair
+        </Button>
+      </div>
+    </div>
   )
 }
 
