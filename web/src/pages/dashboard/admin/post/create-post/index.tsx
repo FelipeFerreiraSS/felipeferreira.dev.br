11fd36d0e4f36613633769dca1870f981835863d
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2024-09-17 23:05:25 -0300

    finishing the create post screen, adding tags and image and react-select

--- web/src/pages/dashboard/admin/post/create-post/index.tsx
+++ web/src/pages/dashboard/admin/post/create-post/index.tsx
@@ -1,42 +1,63 @@
 import HeaderMenu from "@/components/headerMenu";
 import { Button } from "@/components/ui/button";
+import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
+import Uploader from "@/components/uploader";
 import { useToast } from "@/hooks/use-toast";
+import { fetchImagesList } from "@/store/features/image/truckFunctions";
 import { createPost, fetchPostsList } from "@/store/features/post/truckFunctions";
-import { AppDispatch } from "@/store/store";
+import { fetchTagsList } from "@/store/features/tag/truckFunctions";
+import { AppDispatch, RootState } from "@/store/store";
+import { Image  as ImageType } from "@/types/Image";
+import Image from "next/image";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import Select from 'react-select';
 
 export type createPostType = {
   title: string,
   slug: string,
   published: boolean,
-  headerImageId: number,
+  headerImageId: number | undefined,
   summary: string,
   content: string,
-  tags: []
+  tags: string[],
 }
 
 export default function CreatePost() {
+  const [slug, setSlug] = useState('')
+  const [selectedTags, setSelectedTags] = useState<string[]>([]);
+  const [selectedImage, setSelectedImage] = useState<{ id: number; imageUrl: string } | null>(null);
+  
+  const tagsState = useSelector((state: RootState) => state.tags)
+  const imagesState = useSelector((state: RootState) => state.images.images)
+
   const {
     register,
     handleSubmit,
-    control,
     watch,
     setValue,
-    unregister,
     formState: { errors },
   } = useForm<createPostType>()
+
   const router = useRouter()
   const { toast } = useToast()
+
   const dispatch: AppDispatch = useDispatch()
 
-  const [slug, setSlug] = useState('')
+  const options = tagsState?.tags?.map(tag => ({
+    value: tag.name,
+    label: tag.name
+  }));
 
+  const handleImageClick = (image: ImageType) => {
+    setSelectedImage({ id: image.id, imageUrl: image.imageUrl });
+  };
+  
   const generateSlug = (title: string) => {
     return title
       .toLowerCase()
@@ -51,7 +72,9 @@ export default function CreatePost() {
   async function handleCreatePost(data: createPostType) {
     const dataPost = {
       ...data,
-      slug: slug
+      slug: slug,
+      tags: selectedTags,
+      headerImageId: selectedImage?.id
     }
     console.log(dataPost);
     
@@ -76,7 +99,6 @@ export default function CreatePost() {
     setValue('published', true);
   };
 
-  // Função para lidar com o botão "Salvar"
   const handleSave = () => {
     setValue('published', false);
   };
@@ -84,6 +106,11 @@ export default function CreatePost() {
   useEffect(() => {
     setSlug(generateSlug(title))
   }, [title])
+
+  useEffect(() => {
+    dispatch(fetchTagsList())
+    dispatch(fetchImagesList())
+  }, [])
   
   return(
     <>
@@ -99,11 +126,62 @@ export default function CreatePost() {
               Voltar
             </Button>
           </div>
+          <div>
+            <Label htmlFor="title">Imagem de capa</Label>
+            <Dialog>
+              <DialogTrigger>
+                {selectedImage ? (
+                  <Image
+                    src={selectedImage.imageUrl}
+                    width={300}
+                    height={300}
+                    alt="Picture of the author"
+                    className="rounded-xl"
+                  />
+                ) : (
+                  <div className="bg-slate-200 w-96 h-32 flex justify-center items-center rounded-xl">
+                    <h3>Adicione uma imagem de capa</h3>
+                  </div>
+                )}
+              </DialogTrigger>
+              <DialogContent className="max-w-3xl">
+                <DialogHeader>
+                  <DialogTitle className="mb-3">Selecione uma imagem para capa</DialogTitle>
+                  <DialogDescription>
+                    <div>
+                      <div className="mb-3">
+                        <Uploader />
+                      </div>
+                      <div className="grid grid-cols-5 gap-2">
+                        {imagesState?.map((image) => (
+                          <div 
+                            key={image.id}
+                            onClick={() => handleImageClick(image)}
+                          >
+                            <Image
+                              src={image.imageUrl}
+                              width={200}
+                              height={200}
+                              alt="Picture of the author"
+                              className={`rounded-xl mb-3 cursor-pointer ${selectedImage?.id === image.id ? 'border-4 border-blue-500' : ''}`}
+                            />
+                          </div>
+                        ))}
+                      </div>
+                    </div>
+                  </DialogDescription>
+                </DialogHeader>
+                <DialogFooter className="sm:justify-end">
+                  <DialogClose asChild>
+                    <Button type="button" variant="default" className="bg-blue-500 ">
+                      Salvar
+                    </Button>
+                  </DialogClose>
+                </DialogFooter>
+              </DialogContent>
+            </Dialog>
+          </div>
           <form className="space-y-6" onSubmit={handleSubmit(handleCreatePost)}>
-            <div>
-              <Label htmlFor="title">Imagem de capa</Label>
-              {/* <Uploader /> */}
-            </div>
             <div>
               <Label htmlFor="title">Titulo</Label>
               <Input 
@@ -115,6 +193,23 @@ export default function CreatePost() {
                 name="title"
               />
               <p>Slug: {slug}</p>
+            </div>
+            <div>
+              <Label htmlFor="type">Tags</Label>
+              <Select
+                //defaultValue={[colourOptions[2], colourOptions[3]]}
+                isMulti
+                name="tags"
+                options={options}
+                className="basic-multi-select"
+                classNamePrefix="select"
+                placeholder='-- Selecione --'
+                noOptionsMessage={() => "Nenhuma opção encontrada"}
+                onChange={(selectedOptions) => {
+                  const selectedTags = selectedOptions.map(option => option.value);
+                  setSelectedTags(selectedTags);
+                }}
+              />
             </div>
             <div>
               <Label htmlFor="summary">Resumo</Label>
